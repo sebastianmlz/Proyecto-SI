bfc67e5902c43e692617514206318d15b6885aa5
sebastianmlz_07 <sebamendex11@gmail.com>
2024-09-17 19:05:09 -0400

    Primer commit - Proyecto SI

---
+++ src/routes/Login.svelte
@@ -0,0 +1,16 @@
+<script>
+
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<section>
+
+</section>
+
+<style>
+
+</style>
